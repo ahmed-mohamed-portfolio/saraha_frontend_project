71118d0b86b9a358565894d54b50309fccd4de0d
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-03-05 11:47:14 +0200

    login api finished

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
@@ -10,7 +10,8 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: "top" }), withViewTransitions()),
+    provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
         preset: Aura
--- src/app/core/services/api/auth.service.ts
+++ src/app/core/services/api/auth.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 import { RegisterRes } from '../../models/register-res';
 import { Register } from '../../models/register';
+import { LoginRes } from '../../models/login-res';
+import { Login } from '../../models/login';
 
 @Injectable({
   providedIn: 'root',
@@ -13,10 +15,12 @@ export class AuthService {
   http: HttpClient = inject(HttpClient)
 
   signUp(data: Register): Observable<RegisterRes> {
-
-    console.log(environment.baseUrl + '/auth/signup');
     return this.http.post<any>(environment.baseUrl + '/auth/signup', data);
+  }
 
+
+  login(data: Login): Observable<LoginRes> {
+    return this.http.post<any>(environment.baseUrl + '/auth/login', data);
   }
 
 }
