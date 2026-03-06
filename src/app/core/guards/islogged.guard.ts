55e9db199aa35e12a689ce5c80783e96f76c09a6
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-03-06 15:13:33 +0200

    enh

--- src/app/core/guards/islogged.guard.ts
+++ src/app/core/guards/islogged.guard.ts
@@ -1,13 +1,11 @@
 import { CookieService } from 'ngx-cookie-service';
 import { CanActivateFn, Router } from '@angular/router';
-import { inject, PLATFORM_ID } from '@angular/core';
+import { inject } from '@angular/core';
 
 export const isloggedGuard: CanActivateFn = (route, state) => {
 
   const cookieService = inject(CookieService)
   const router = inject(Router)
-
-  console.log(cookieService.get("accessToken"));
 
   if (cookieService.get("accessToken")) {
     return router.parseUrl('messages')
